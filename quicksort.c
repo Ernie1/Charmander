622958f5ece0626766bc843d0994978509f2da93
Ernie1 <870949712@qq.com>
2018-05-18 11:41:08 +0800

    progr

--- quicksort.c
+++ quicksort.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 void swap(int array[], int left, int right)
 {
     int temp = array[left];
@@ -47,11 +46,13 @@ void recursion(int array[], int left, int right)
     }
 }
 
+#include <stdio.h>
+
 int main()
 {
-    int array[] = {34, -2, 43, -3, 3, 0, 22, 56, 30, -10};
-    recursion(array, 0, 9);
-    for (int i = 0; i < 10; ++i)
+    int array[] = {50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+    recursion(array, 0, 49);
+    for (int i = 0; i < 50; ++i)
         printf("%d ", array[i]);
     printf("\n");
     return 0;
